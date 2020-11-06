acce17e30cd286876c3ad2eff645728d61babd72
Arne Coucheron <arco68@gmail.com>
2020-11-06 15:22:40 -0500

    SF: Don't cleanup resources from previous frame
    Causes bad lag on some legacy devices.
    
    Change-Id: I89913d214c7377c73bd307696dbf9aac2f9a5c0a
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3677,12 +3677,8 @@ void SurfaceFlinger::onCompositionPresented(PhysicalDisplayId pacesetterId,
     mQtiSFExtnIntf->qtiUpdateSmomoState();
 // QTI_END: 2023-03-06: Display: SF: Squash commit of SF Extensions.
     if (hasPacesetterDisplay && !pacesetterDisplay->isPoweredOn()) {
-        getRenderEngine().cleanupPostRender();
         return;
     }
-
-    // Cleanup any outstanding resources due to rendering a prior frame.
-    getRenderEngine().cleanupPostRender();
 
     if (mNumTrustedPresentationListeners > 0) {
         // We avoid any reverse traversal upwards so this shouldn't be too expensive
