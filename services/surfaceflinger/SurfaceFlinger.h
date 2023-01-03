f817dc61411f7f3501871ff3dd5881eebf815071
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-03 17:09:14 +0000

    SF: Make "Get latest vsync period from HWC" optional
    Devices with older display HAL seem to return incorrect vsync period
    after wakeup, which caused massive lags after 91f07729. Allow devices
    to revert back to the older behaviour by setting
    ro.sf.use_latest_hwc_vsync_period=0 in system prop.
    
    Change-Id: Iba81f6981abd514ce5887fba6320480f2d906185

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1561,6 +1561,7 @@ private:
     bool mLumaSampling = true;
     bool mForceLightBrightness = false;
     bool mForceHwcBrightness = false;
+    bool mUseLatestHwcVsyncPeriod = true;
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;
