a4686a51923e5833c08faa6cf1813f45708febd2
Vinoth Jayaram <quic_c_vinoja@quicinc.com>
2024-04-03 15:36:26 +0530

    SF: Fix issue with async Virtual Display creation
    Do not return early and make sure to invoke qtiCreateVirtualDisplay
    to create async Virtual display if it's enabled.
    
    CRs-Fixed: 3781590
    Change-Id: I89c12b11d376a56520640967149182cdc54aa97f

--- services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.cpp
+++ services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.cpp
@@ -1160,7 +1160,6 @@ void QtiSurfaceFlingerExtension::qtiCheckVirtualDisplayHint(const Vector<Display
                             ALOGW_IF(status != NO_ERROR, "Unable to query usage (%d)", status);
                             if ((status == NO_ERROR) && qtiCanAllocateHwcDisplayIdForVDS(usage)) {
                                 createVirtualDisplay = true;
-                                return;
                             }
                         }
                     }
