9ddb1cc52ae728b9c5ee2fd2dab7357d7a9024c7
Richard Raya <rdxzv.dev@gmail.com>
2025-01-12 22:59:07 -0300

    Revert "Merge tag 'LA.QSSI.15.0.r1-12900-qssi.0' of https://git.codelinaro.org/clo/la/platform/frameworks/native into HEAD"
    This reverts commit a500fd5995920fcb9ab0019ced9bc67e8a633c04, reversing
    changes made to 8bca86d2853b997820b874ea3607b4ecf1a28cbc.

--- services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.h
+++ services/surfaceflinger/QtiExtension/QtiSurfaceFlingerExtension.h
@@ -14,7 +14,6 @@
 #include <composer_extn_intf.h>
 #include <list>
 #include <map>
-#include <set>
 
 #include "../DisplayHardware/HWComposer.h"
 #include "../DisplayHardware/PowerAdvisor.h"
@@ -258,23 +257,6 @@ public:
                                      const DisplayDeviceState& drawingState) override;
     void qtiFbScalingOnPowerChange(sp<DisplayDevice> display) override;
 
-    /*
-     * Methods for multiple displays
-     */
-    // enable/disable h/w composer event
-    // TODO: this should be made accessible only to EventThread
-    // main thread function to enable/disable h/w composer event
-    sp<DisplayDevice> qtiGetVsyncSource();
-    void qtiUpdateVsyncSource();
-    nsecs_t qtiGetVsyncPeriodFromHWC() const;
-    void qtiUpdateNextVsyncSource();
-    void qtiUpdateActiveVsyncSource();
-    bool qtiIsDummyDisplay(const sp<DisplayDevice>& display);
-    void qtiUpdateActiveDisplayOnRemove(PhysicalDisplayId id);
-    void qtiUpdateActiveDisplayOnPowerOn(PhysicalDisplayId id);
-    void qtiUpdateActiveDisplayOnPowerOff(PhysicalDisplayId id);
-    sp<DisplayDevice> qtiGetVsyncSourceForFence();
-
 private:
     SmomoIntf* qtiGetSmomoInstance(const uint32_t layerStackId) const;
     bool qtiIsInternalDisplay(const sp<DisplayDevice>& display);
@@ -312,7 +294,7 @@ private:
     bool mQtiAllowThermalFpsChange = false;
     bool mQtiRequestedContentFps = false;
     int mQtiFailedAttempts = 0;
-    std::set<uint32_t> mQtiHasScreenshotSet;
+    bool mQtiHasScreenshot = false;
 
     std::shared_ptr<IDisplayConfig> mQtiDisplayConfigAidl = nullptr;
     std::shared_ptr<DisplayConfigAidlCallbackHandler> mQtiAidlCallbackHandler = nullptr;
@@ -343,10 +325,6 @@ private:
     std::unordered_map<DisplayId, VisibleLayerInfo> mQtiVisibleLayerInfoMap;
 
     std::vector<SmomoInfo> mQtiSmomoInstances{};
-
-    sp<DisplayDevice> mQtiActiveVsyncSource = NULL;
-    sp<DisplayDevice> mQtiNextVsyncSource = NULL;
-    mutable std::recursive_mutex mQtiVsyncLock;
 };
 
 } // namespace android::surfaceflingerextension
